6366c1c6d35066a81ed18a5135381bf332543917
munir <smsirajulmonir@gmail.com>
2024-06-20 19:29:46 -0700

    craft update api added

--- index.js
+++ index.js
@@ -44,6 +44,41 @@ async function run() {
       const crafts = await cursor.toArray();
       res.send(crafts);
     })
+    app.get('/craft/idx/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await craftCollection.findOne(query);
+      res.send(result);
+    })
+
+    // read for update
+    app.get('/craft/update/:id', async(req, res)=>{
+      const id = req.params.id;
+      console.log(id)
+      const query = {_id: new ObjectId(id)}
+      const result = await craftCollection.findOne(query);
+      res.send(result);
+    })
+    // update craft
+    app.put('/craft/update/:id', async(req, res)=>{
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id)}
+      const options = { upsert: true }
+      const updatedCraft =req.body;
+      console.log(updatedCraft)
+      const craft = {
+        $set: {
+          user_name: updatedCraft.user_name, user_email:updatedCraft.user_email,
+           subcategory_Name: updatedCraft.subcategory_Name,stockStatus: updatedCraft.stockStatus,
+           short_description: updatedCraft.short_description,rating:updatedCraft.rating,
+            processing_time:updatedCraft.processing_time, 
+            price:updatedCraft.price, item_name:updatedCraft.price,
+             image: updatedCraft.image, customization:updatedCraft.customization
+        }
+      }
+      const result = await craftCollection.updateOne(filter, craft, options);
+      res.send(result)
+    })
 
     // delete operation
     app.delete('/craft/:id', async(req, res) =>{
@@ -52,7 +87,6 @@ async function run() {
       const result = await craftCollection.deleteOne(query);
       res.send(result);
     })
-
     app.get('/craft/:email', async(req, res) =>{
       const email = req.params.email;
       const query = { user_email: email };
@@ -69,16 +103,28 @@ async function run() {
       console.log(result)
       res.send(result)
     })
-
     app.post('/craft', async(req, res) =>{
-        const craft = req.body;
-        const result = await craftCollection.insertOne(craft);
+      const craft = req.body;
+      const result = await craftCollection.insertOne(craft);
+      res.send(result);
+    })
+
+    // user related api
+    app.get('/user', async(req, res) =>{
+        const cursor = userCollection.find();
+        const users = await cursor.toArray();
+        res.send(users);
+    })
+
+    app.post('/user', async(req, res)=>{
+        const user = req.body;
+        console.log(user);
+        const result = await userCollection.insertOne(user);
         res.send(result);
-      })
-  
+    })
 
 
-    
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
